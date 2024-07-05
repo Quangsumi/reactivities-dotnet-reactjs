005225d27d9896c79db57531d43b61035225b483
djquangsumi@gmail.com <djquangsumi@gmail.com>
2024-07-05 16:09:24 +0700

    end of section 20

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -9,6 +9,11 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({attendees}: Props) {
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 3
+    }
+
     return (
         <List horizontal>
             {attendees.map(attendee => (
@@ -18,8 +23,10 @@ export default observer(function ActivityListItemAttendee({attendees}: Props) {
                     trigger={
                         <List.Item as={Link} to={`/profiles/${attendee.username}`}>
                             <Image size='mini'
-                                   circular
-                                   src={attendee.image || `/assets/user.png`} />
+                                style={attendee.following ? styles : null}
+                                bordered
+                                circular
+                                src={attendee.image || `/assets/user.png`} />
                         </List.Item>
                     }
                 >
